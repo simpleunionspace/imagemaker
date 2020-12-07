6b935127bd3056979f26f33ebf80294df763e979
Pavel Kurkin <kurkinps@kurkin-family.net>
2020-12-08 01:20:51 +0300

    feature/rework: now worked on packer

--- make/base-linux/make.ps1
+++ make/base-linux/make.ps1
@@ -2,26 +2,20 @@ Set-StrictMode -Version 3
 
 ${BasePath} = ${PSScriptRoot}
 
-# ubuntu
-## 20.04
-docker build `
-       --pull `
-       --no-cache `
-       --build-arg IMAGE_BASE_NAME=ubuntu `
-       --build-arg IMAGE_BASE_VERSION=20.04 `
-       --build-arg SYSTEM_PACKAGES_MANAGER=apt `
-       --tag simpleunionspace/base:linux-ubuntu-20.04-$(Get-date -Format yyyyMMdd.HHmm) `
-       --tag simpleunionspace/base:linux-ubuntu-20.04-latest `
-       ${BasePath}/../../src/base-linux/
+${ImageTagSuffix}=$(Get-Date -Format yyyyMMdd.HHmm)
 
-# centos
-## 8
-docker build `
-       --pull `
-       --no-cache `
-       --build-arg IMAGE_BASE_NAME=centos `
-       --build-arg IMAGE_BASE_VERSION=8 `
-       --build-arg SYSTEM_PACKAGES_MANAGER=yum `
-       --tag simpleunionspace/base:linux-centos-8-$(Get-date -Format yyyyMMdd.HHmm) `
-       --tag simpleunionspace/base:linux-centos-8-latest `
-       ${BasePath}/../../src/base-linux/
+${VarFiles} = @("$(Convert-Path $(Join-Path ${BasePath} '/../../src/linux-ubuntu-1804.json'))",
+                "$(Convert-Path $(Join-Path ${BasePath} '/../../src/linux-ubuntu-2004.json'))",
+                "$(Convert-Path $(Join-Path ${BasePath} '/../../src/linux-centos-7.json'))",
+                "$(Convert-Path $(Join-Path ${BasePath} '/../../src/linux-centos-8.json'))")
+
+
+
+foreach (${VarFile} in ${VarFiles})
+{
+    packer build `
+           -force `
+           -var-file="${VarFile}" `
+           -var "image_tag_suffix=${ImageTagSuffix}" `
+           ${BasePath}/../../src/base-linux/template.json
+}
