c7c3f577a6bfc9ec8772b9d7c24dae3bb31bd46c
Pavel Kurkin <kurkinps@kurkin-family.net>
2020-12-12 12:14:52 +0300

    master: added publish scripts

---
+++ make/base-linux/publish.ps1
@@ -0,0 +1,5 @@
+Set-StrictMode -Version 3
+
+${BasePath} = ${PSScriptRoot}
+
+docker push --all-tags simpleunionspace/base
---
+++ make/publish.ps1
@@ -0,0 +1,6 @@
+Set-StrictMode -Version 3
+
+${BasePath} = ${PSScriptRoot}
+
+# base linux
+powershell ${BasePath}/base-linux/publish.ps1
